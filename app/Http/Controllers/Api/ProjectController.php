a98bd2650702f66b331657a4b32bdbda33dbd86f
NicholasBuiatti <nic.buiatti@gmail.com>
2024-07-22 17:19:06 +0200

    creazione rotta per lo show-chiamata

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,24 @@ class ProjectController extends Controller
     public function index()
     {
         return response()->json([
-            "projects" => Project::with(['type'])->orderByDesc('id')->paginate(4)
+            "projects" => Project::with(['type'])->orderByDesc('id')->paginate(6)
         ]);
+    }
+
+    public function show($id)
+    {
+        $project = Project::with('type')->where('id', $id)->first();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'not found'
+            ]);
+        }
     }
 }
