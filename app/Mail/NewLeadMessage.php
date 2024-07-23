b55257a7d51fd0c7db008b06ba0526baa4874062
NicholasBuiatti <nic.buiatti@gmail.com>
2024-07-23 16:32:05 +0200

    creazzione lead con -mmc e creazione NewLeadMessage con make:mail e view

---
+++ app/Mail/NewLeadMessage.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewLeadMessage extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $lead;
+    public function __construct($lead)
+    {
+        $this->lead = $lead;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Lead Message',
+            replyTo: $this->lead->message,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'make:mail',
+            view: 'mail.NewLeadMessage'
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
