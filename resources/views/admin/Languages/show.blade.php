db730d35fcba59d41a234877d7c24c48eb3ae5db
NicholasBuiatti <nic.buiatti@gmail.com>
2024-11-06 17:23:32 +0100

    fix

--- resources/views/admin/Languages/show.blade.php
+++ resources/views/admin/Languages/show.blade.php
@@ -1,11 +1,10 @@
-@extends('layouts.admin')
+@extends('layouts.NavAdmin')
 
 @section('content')
 	<h1 class="text-center mb-5">{{ $type->name }}</h1>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">{{ $type->name }}</h5>
-			<h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+			<h5 class="card-title fs-1">{{ $type->name }}</h5>
 			<p class="card-text">{{ $type->description }}</p>
 			<a href="#" class="card-link">Articolo {{ $type->name }}</a>
 		</div>
